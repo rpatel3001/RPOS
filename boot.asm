c2b6f2aa47814ca3908e27f2314b3d1789e9135f
Rajan Patel <rpatel3001@gmail.com>
2017-01-27 15:10:43 -0500

    keyboard input is a go

--- boot.asm
+++ boot.asm
@@ -37,6 +37,34 @@ stack_top:
 ; doesn't make sense to return from this function as the bootloader is gone.
 ; Declare _start as a function symbol with the given symbol size.
 section .text
+
+global keyboard_handler
+global read_port
+global write_port
+global load_idt
+extern keyboard_handler_main
+
+read_port:
+	mov edx, [esp + 4]
+	in al, dx
+	ret
+
+write_port:
+	mov   edx, [esp + 4]
+	mov   al, [esp + 4 + 4]
+	out   dx, al
+	ret
+
+load_idt:
+	mov edx, [esp + 4]
+	lidt [edx]
+	sti
+	ret
+
+keyboard_handler:                 
+	call    keyboard_handler_main
+	iretd
+
 global _start:function (_start.end - _start)
 _start:
 	; The bootloader has loaded us into 32-bit protected mode on a x86
--- kernel.c
+++ kernel.c
@@ -2,6 +2,8 @@
 #include <stddef.h>
 #include <stdint.h>
 
+#include "keyboard_map.h"
+
 /* Check if the compiler thinks we are targeting the wrong operating system. */
 #if defined(__linux__)
 #error "You are not using a cross-compiler, you will most certainly run into trouble"
@@ -11,6 +13,108 @@
 #if !defined(__i386__)
 #error "This tutorial needs to be compiled with a ix86-elf compiler"
 #endif
+
+extern void keyboard_handler(void);
+extern char read_port(unsigned short port);
+extern void write_port(unsigned short port, unsigned char data);
+extern void load_idt(unsigned long *idt_ptr);
+void terminal_putchar(char c);
+#define KEYBOARD_DATA_PORT 0x60
+#define KEYBOARD_STATUS_PORT 0x64
+#define IDT_SIZE 256
+void keyboard_handler_main(void) {
+	unsigned char status;
+	char keycode;
+
+	/* write EOI */
+	write_port(0x20, 0x20);
+
+	status = read_port(KEYBOARD_STATUS_PORT);
+	/* Lowest bit of status will be set if buffer is not empty */
+	if (status & 0x01) {
+		keycode = read_port(KEYBOARD_DATA_PORT);
+		if (keycode < 0)
+			return;
+		terminal_putchar(keyboard_map[(unsigned char)keycode]);
+	}
+}
+
+void kb_init(void) {
+	/* 0xFD is 11111101 - enables only IRQ1 (keyboard)*/
+	write_port(0x21 , 0xFD);
+
+	//ensure the keyboard is using scancode set 2
+	write_port(KEYBOARD_DATA_PORT, 0xF0);
+	write_port(KEYBOARD_DATA_PORT, 0x2);
+}
+
+struct IDT_entry {
+	unsigned short int offset_lowerbits;
+	unsigned short int selector;
+	unsigned char zero;
+	unsigned char type_attr;
+	unsigned short int offset_higherbits;
+};
+
+struct IDT_entry IDT[IDT_SIZE];
+
+void idt_init(void) {
+	unsigned long keyboard_address;
+	unsigned long idt_address;
+	unsigned long idt_ptr[2];
+
+	/* populate IDT entry of keyboard's interrupt */
+	keyboard_address = (unsigned long)keyboard_handler;
+	IDT[0x21].offset_lowerbits = keyboard_address & 0xffff;
+	IDT[0x21].selector = 0x08; /* KERNEL_CODE_SEGMENT_OFFSET */
+	IDT[0x21].zero = 0;
+	IDT[0x21].type_attr = 0x8e; /* INTERRUPT_GATE */
+	IDT[0x21].offset_higherbits = (keyboard_address & 0xffff0000) >> 16;
+
+
+	/*     Ports
+	*	 PIC1	PIC2
+	*Command 0x20	0xA0
+	*Data	 0x21	0xA1
+	*/
+
+	/* ICW1 - begin initialization */
+	write_port(0x20 , 0x11);
+	write_port(0xA0 , 0x11);
+
+	/* ICW2 - remap offset address of IDT */
+	/*
+	* In x86 protected mode, we have to remap the PICs beyond 0x20 because
+	* Intel have designated the first 32 interrupts as "reserved" for cpu exceptions
+	*/
+	write_port(0x21 , 0x20);
+	write_port(0xA1 , 0x28);
+
+	/* ICW3 - setup cascading */
+	write_port(0x21 , 0x00);
+	write_port(0xA1 , 0x00);
+
+	/* ICW4 - environment info */
+	write_port(0x21 , 0x01);
+	write_port(0xA1 , 0x01);
+	/* Initialization finished */
+
+	/* mask interrupts */
+	write_port(0x21 , 0xff);
+	write_port(0xA1 , 0xff);
+
+	/* fill the IDT descriptor */
+	idt_address = (unsigned long)IDT ;
+	idt_ptr[0] = (sizeof (struct IDT_entry) * IDT_SIZE) + ((idt_address & 0xffff) << 16);
+	idt_ptr[1] = idt_address >> 16 ;
+
+	load_idt(idt_ptr);
+}
+
+
+
+
+
 
 /* Hardware text mode color constants. */
 enum vga_color {
@@ -107,7 +211,7 @@ void terminal_putchar(char c) {
 		}
 	} else if (c == 9) {
 		//tab
-		for(; terminal_column % 8 != 0;) {
+		for (; terminal_column % 8 != 0;) {
 			terminal_putchar(' ');
 		}
 	} else if (c == 10) {
@@ -121,6 +225,12 @@ void terminal_putchar(char c) {
 	} else if (c == 13) {
 		//carriage return
 		terminal_column = 0;
+	} else if (c == 127) {
+		//delete
+		if (terminal_column != 0) {
+			--terminal_column;
+			linecpy(&terminal_buffer[index-1], &terminal_buffer[index], index - index / VGA_WIDTH * terminal_row);
+		}
 	} else {
 		//printable character
 		terminal_putentryat(c, terminal_color, terminal_column, terminal_row);
@@ -151,4 +261,10 @@ void kernel_main(void) {
 	}
 	terminal_writestring("Hello, kernel World!\r");
 	terminal_writestring("Blech\bk\nbye\tkern\na\tb\tc\nasdfagfdsfgvd\tfdfeac");
+
+
+	idt_init();
+	kb_init();
+
+	while (1);
 }
