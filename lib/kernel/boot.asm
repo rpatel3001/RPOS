48d33086c002825a8c26ca2e00b8a7ecec5e65e4
Rajan Patel <rpatel3001@gmail.com>
2017-02-10 00:45:41 -0500

    clean out boot.asm

--- lib/kernel/boot.asm
+++ lib/kernel/boot.asm
@@ -1,19 +1,14 @@
 ; Declare constants for the multiboot header.
-ARCHITECTURE equ 0x00000000
-HEADER_LEN equ header_end - header_start
-MAGIC    equ  0xE85250D6
-CHECKSUM equ -(MAGIC + ARCHITECTURE + HEADER_LEN)
+ARCH 		equ 0x00000000
+HEADER_LEN 	equ header_end - header_start
+MAGIC    	equ  0xE85250D6
+CHECKSUM 	equ -(MAGIC + ARCH + HEADER_LEN)
 
-; Declare a multiboot header that marks the program as a kernel. These are
-; magic values that are documented in the multiboot standard. The bootloader
-; will search for this signature in the first 8 KiB of the kernel file,
-; aligned at a 32-bit boundary. The signature is in its own section so the 
-; header can be forced to be within the first 8 KiB of the kernel file.
 section .multiboot
 align 4
 header_start:
 	dd MAGIC
-	dd ARCHITECTURE
+	dd ARCH
 	dd HEADER_LEN
 	dd CHECKSUM
 
@@ -23,51 +18,19 @@ header_start:
 	dd 8
 header_end:
 
-; The multiboot standard does not define the value of the stack pointer
-; register (esp) and it is up to the kernel to provide a stack. This allocates
-; room for a small stack by creating a symbol at the bottom of it, then
-; allocating 16384 bytes for it, and finally creating a symbol at the top. The
-; stack grows downwards on x86. The stack is in its own section so it can be
-; marked nobits, which means the kernel file is smaller because it does not
-; contain an uninitialized stack. The stack on x86 must be 16-byte aligned
-; according to the System V ABI standard and de-facto extensions. The compiler
-; will assume the stack is properly aligned and failure to align the stack
-; will result in undefined behavior.
 section .bss
 align 4
 stack_bottom:
 resb 16384 ; 16 KiB
 stack_top:
 
-; The linker script specifies _start as the entry point to the kernel and the
-; bootloader will jump to this position once the kernel has been loaded. It
-; doesn't make sense to return from this function as the bootloader is gone.
-; Declare _start as a function symbol with the given symbol size.
 section .text
 bits 32
 
 global _start:function
 _start:
-	; The bootloader has loaded us into 32-bit protected mode on a x86
-	; machine. Interrupts are disabled. Paging is disabled. The processor
-	; state is as defined in the multiboot standard. The kernel has full
-	; control of the CPU.
- 
-	; To set up a stack, we set the esp register to point to the top of our
-	; stack (as it grows downwards on x86 systems). This is necessarily done
-	; in assembly as languages such as C cannot function without a stack.
 	mov esp, stack_top
- 
-	; This is a good place to initialize crucial processor state before the
-	; high-level kernel is entered. It's best to minimize the early
-	; environment where crucial features are offline. Note that the
-	; processor is not fully initialized yet: Features such as floating
-	; point instructions and instruction set extensions are not initialized
-	; yet. The GDT should be loaded here. Paging should be enabled here.
- 
-	; Enter the high-level kernel. The ABI requires the stack is 16-byte
-	; aligned at the time of the call instruction (which afterwards pushes
-	; the return pointer of size 4 bytes).
+
 	; enable write protect for testing page faults
 	mov edx, cr0
 	bts edx, 16
@@ -75,8 +38,6 @@ _start:
 	extern kernel_main
 	call kernel_main
 
-	; If the system has nothing more to do, put the computer into an
-	; infinite loop.
 	extern asm_halt
 	jmp asm_halt
 .end:
