3e9c50f9be1a5f3a6bacd3b6804b90e0184b78af
Rajan Patel <rpatel3001@gmail.com>
2018-01-01 22:27:41 -0500

    make install script for crosscomiplation tools

---
+++ crosscompiler/install.sh
@@ -0,0 +1,23 @@
+git clone git://sourceware.org/git/binutils-gdb.git
+git clone https://github.com/gcc-mirror/gcc.git
+
+export PREFIX="$HOME/opt/cross"
+export TARGET=i686-elf
+export PATH="$PREFIX/bin:$PATH"
+
+mkdir build-binutils
+cd build-binutils
+../binutils-x.y.z/configure --target=$TARGET --prefix="$PREFIX" --with-sysroot 
+--disable-nls --disable-werror
+make
+make install
+
+cd ..
+mkdir build-gcc
+cd build-gcc
+../gcc-x.y.z/configure --target=$TARGET --prefix="$PREFIX" --disable-nls 
+--enable-languages=c,c++ --without-headers
+make all-gcc
+make all-target-libgcc
+make install-gcc
+make install-target-libgcc
