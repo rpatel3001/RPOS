d01870d1c59ed9c3663e4d3bd3e488b3fe68d900
Rajan Patel <rpatel3001@gmail.com>
2020-11-28 19:12:00 -0500

    mk

--- crosscompiler/install.sh
+++ crosscompiler/install.sh
@@ -7,16 +7,14 @@ export PATH="$PREFIX/bin:$PATH"
 
 mkdir build-binutils
 cd build-binutils
-../binutils-gdb/configure --target=$TARGET --prefix="$PREFIX" --with-sysroot 
---disable-nls --disable-werror
+../binutils-gdb/configure --target=$TARGET --prefix="$PREFIX" --with-sysroot --disable-nls --disable-werror
 make
 make install
 
 cd ..
 mkdir build-gcc
 cd build-gcc
-../gcc/configure --target=$TARGET --prefix="$PREFIX" --disable-nls 
---enable-languages=c,c++ --without-headers
+../gcc/configure --target=$TARGET --prefix="$PREFIX" --disable-nls --enable-languages=c,c++ --without-headers
 make all-gcc
 make all-target-libgcc
 make install-gcc
---
+++ mk.sh
@@ -0,0 +1,3 @@
+PATH="$HOME/opt/cross/bin:$PATH"
+
+make $1
